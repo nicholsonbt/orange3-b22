c89f65bc6b041ce6aa695f1a01351c52c44ae0dd
Ben Nicholson <86907890+nicholsonbt@users.noreply.github.com>
2024-07-25 16:36:58 +0100

    Correct .gsf file reader

--- orangecontrib/b22/io/gwyddion.py
+++ orangecontrib/b22/io/gwyddion.py
@@ -1,11 +1,10 @@
 import numpy as np
-#import gwyfile
 
 from Orange.data import FileFormat
 
 from orangecontrib.spectroscopy.io.util import SpectralFileFormat, _spectra_from_image
 
-from orangecontrib.b22.io.utils import MetaFormatter, transform_row_col
+from orangecontrib.b22.io.utils import transform_row_col
 
 
 
@@ -31,8 +30,6 @@ def reader_gsf(file_path):
 
         f.read(4 - f.tell() % 4)
 
-        print(meta.keys())
-
         meta["XRes"] = XR = int(meta["XRes"])
         meta["YRes"] = YR = int(meta["YRes"])
         meta["XReal"] = float(meta.get("XReal", 1))
@@ -43,66 +40,10 @@ def reader_gsf(file_path):
         meta["XYUnits"] = meta.get("XYUnits", None)
         meta["ZUnits"] = meta.get("ZUnits", None)
 
-        metas = {
-            "Real Center" : {
-                "X" : float(meta.get("XOffset", 0.0)) + float(meta.get("XReal", 1.0)) / 2,
-                "Y" : float(meta.get("YOffset", 0.0)) + float(meta.get("XReal", 1.0)) / 2,
-            },
-
-            "Angle" : {
-                "Theta" : float(meta.get("Neaspec_Angle", 0.0))
-            },
-
-            "Real Area" : {
-                "X" : float(meta.get("XReal", 1.0)),
-                "Y" : float(meta.get("YReal", 1.0)),
-            },
-
-            "Pixel Area" : {
-                "X" : float(meta.get("XRes", 1.0)),
-                "Y" : float(meta.get("YRes", 1.0)),
-            }
-        }
-
-
-
-
-        ## Assume XOffset, YOffset is the bottom left corner of the map.
-
         X = np.fromfile(f, dtype='float32', count=XR*YR).reshape(XR, YR)
 
-        XRr = np.linspace(meta["XOffset"], meta["XOffset"] + meta["XReal"], meta["XRes"])
-
-        #YRr = np.linspace(meta["YOffset"] + meta["YReal"], meta["YOffset"], meta["YRes"])
-
-
-        XRr = np.arange(XR) #np.tile(np.arange(XR), YR)
-        YRr = np.arange(YR)[::-1] #np.repeat(np.arange(YR), XR)
-
-        # meta_data = np.vstack((rows, cols)).T
-
-
-        # meta_data = transform_row_col(meta_data, metas)
-
-
-        #XRr, YRr = meta_data.T
-
-        # print(meta_data)
-        # metas = [Orange.data.ContinuousVariable.make("map_x"),
-        #          Orange.data.ContinuousVariable.make("map_y"),
-        #          Orange.data.DiscreteVariable.make("channel", values=channels)]
-
-        # domain = Orange.data.Domain([], None, metas=metas)
-        # meta_data = Table.from_numpy(domain, X=np.zeros((len(M), 0)),
-        #                              metas=meta_data)
-        
-        # meta_data.attributes = meta
-        # return waveN, M, meta_data
-
-
-
-        #XRr = np.arange(XR)
-        #YRr = np.arange(YR-1, -1, -1)  # needed to have the same orientation as in Gwyddion
+        XRr = np.arange(XR)
+        YRr = np.arange(YR)
 
         X = X.reshape((meta["YRes"], meta["XRes"]) + (1,))
 
@@ -124,7 +65,7 @@ class GWYReader(FileFormat, SpectralFileFormat):
         metas = {
             "Real Center" : {
                 "X" : float(meta.get("XOffset", 0.0)) + float(meta.get("XReal", 1.0)) / 2,
-                "Y" : float(meta.get("YOffset", 0.0)) + float(meta.get("XReal", 1.0)) / 2,
+                "Y" : float(meta.get("YOffset", 0.0)) + float(meta.get("YReal", 1.0)) / 2,
             },
 
             "Angle" : {
@@ -148,7 +89,6 @@ class GWYReader(FileFormat, SpectralFileFormat):
     
 if __name__ == "__main__":
     from Orange.data.table import dataset_dirs
-    #reader = GWYReader()
     filename = None
     reader = GWYReader(FileFormat.locate(filename, dataset_dirs))
 
